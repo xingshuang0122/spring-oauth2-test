d04387f6c5101f46676e75ddc27e965fa349304d
Shuang <xingshuang_cool@163.com>
2019-09-19 18:03:14 +0800

    feat(spring-influxdb-demo): 添加influxdb模块;

---
+++ spring-influxdb-demo/src/main/java/com/huibo/influxdb/demo/SpringInfluxdbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.huibo.influxdb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInfluxdbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInfluxdbDemoApplication.class, args);
+    }
+
+}
