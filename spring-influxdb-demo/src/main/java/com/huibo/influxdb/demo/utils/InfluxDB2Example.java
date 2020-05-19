0840fa1a7305d87e4eab663ab7859cd83ee624b5
Shuang <xingshuang_cool@163.com>
2020-05-19 15:54:17 +0800

    feat(spring-influxdb-demo): 采用新的依赖;

---
+++ spring-influxdb-demo/src/main/java/com/huibo/influxdb/demo/utils/InfluxDB2Example.java
@@ -0,0 +1,75 @@
+/*
+  Copyright (C), 2009-2020, 江苏汇博机器人技术股份有限公司
+  FileName: InfluxDB2Example
+  Author:   ShuangPC
+  Date:     2020/5/19
+  Description:
+  History:
+  <author>         <time>          <version>          <desc>
+  作者姓名         修改时间           版本号             描述
+ */
+
+package com.huibo.influxdb.demo.utils;
+
+import com.influxdb.annotations.Column;
+import com.influxdb.annotations.Measurement;
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import com.influxdb.client.QueryApi;
+import com.influxdb.client.WriteApi;
+import com.influxdb.client.domain.WritePrecision;
+import com.influxdb.client.write.Point;
+import com.influxdb.query.FluxRecord;
+import com.influxdb.query.FluxTable;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * @author ShuangPC
+ * @date 2020/5/19
+ */
+
+public class InfluxDB2Example {
+    private static char[] token = "my-token".toCharArray();
+    private static String org = "my-org";
+    private static String bucket = "my-bucket";
+
+    public static void main(final String[] args) {
+
+        String database = "test";
+        String retentionPolicy = "autogen";
+
+        InfluxDBClient client = InfluxDBClientFactory.createV1("http://localhost:8086",
+                "xingshuang",
+                "123456".toCharArray(),
+                database,
+                retentionPolicy);
+
+        System.out.println("*** Write Points ***");
+
+        try (WriteApi writeApi = client.getWriteApi()) {
+
+            Point point = Point.measurement("cpu")
+                    .addTag("host", "serverA")
+                    .addTag("region", "us_west")
+                    .addField("value", 22.43);
+
+            System.out.println(point.toLineProtocol());
+
+            writeApi.writePoint(point);
+        }
+
+        System.out.println("*** Query Points ***");
+        String query = String.format("from(bucket: \"%s/%s\") |> range(start: -1d) |> limit(n:100)", database, retentionPolicy);
+
+        List<FluxTable> tables = client.getQueryApi().query(query);
+        tables.get(0).getRecords()
+                .forEach(record -> System.out.println(String.format("%s %s: %s %s",
+                        Date.from(record.getTime()), record.getMeasurement(), record.getField(), record.getValue())));
+
+        client.close();
+    }
+
+}
--- spring-influxdb-demo/src/test/java/com/huibo/influxdb/demo/SpringInfluxdbDemoApplicationTests.java
+++ spring-influxdb-demo/src/test/java/com/huibo/influxdb/demo/SpringInfluxdbDemoApplicationTests.java
@@ -1,16 +1,9 @@
 package com.huibo.influxdb.demo;
 
-import org.influxdb.BatchOptions;
-import org.influxdb.InfluxDB;
-import org.influxdb.InfluxDBFactory;
-import org.influxdb.dto.Point;
-import org.influxdb.dto.Query;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,34 +16,6 @@ public class SpringInfluxdbDemoApplicationTests {
 
     @Test
     public void test1(){
-        InfluxDB influxDB = InfluxDBFactory.connect("http://127.0.0.1:8086", "admin", "admin");
-        String dbName = "aTimeSeries";
-        influxDB.query(new Query("CREATE DATABASE " + dbName));
-        influxDB.setDatabase(dbName);
-        String rpName = "aRetentionPolicy";
-        influxDB.query(new Query("CREATE RETENTION POLICY " + rpName + " ON " + dbName + " DURATION 30h REPLICATION 2 SHARD DURATION 30m DEFAULT"));
-        influxDB.setRetentionPolicy(rpName);
-
-        influxDB.enableBatch(BatchOptions.DEFAULTS);
-
-        influxDB.write(Point.measurement("cpu")
-                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-                .addField("idle", 90L)
-                .addField("user", 9L)
-                .addField("system", 1L)
-                .build());
-
-        influxDB.write(Point.measurement("disk")
-                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-                .addField("used", 80L)
-                .addField("free", 1L)
-                .build());
-
-//        Query query = new Query("SELECT idle FROM cpu", dbName);
-//        influxDB.query(query);
-//        influxDB.query(new Query("DROP RETENTION POLICY " + rpName + " ON " + dbName));
-//        influxDB.query(new Query("DROP DATABASE " + dbName));
-        influxDB.close();
+//        InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8088", token, org, bucket);
     }
-
 }
